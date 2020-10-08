2f2a5255ad492554658ede62ee6b45d314c56a71
Alesinx <Alejandro7mr@gmail.com>
2020-10-08 21:23:45 +0200

    First commit

---
+++ Source/SaveGame/SaveGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SaveGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAVEGAME_API ASaveGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
